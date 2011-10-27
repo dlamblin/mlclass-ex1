525d4e0612d66de38e8a238a71e6d335cf35c11c
Daniel Lamblin <lamblin@google.com>
2011-10-27 14:45:16 -0400

    Normalizing multiple features

--- featureNormalize.m
+++ featureNormalize.m
@@ -26,13 +26,9 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
-
-
-
-
-
-
-
+mu = mean(X);
+sigma = std(X);
+X_norm = (X-(ones(rows(X),1) * mu)) ./ (ones(rows(X),1) * sigma);
 
 % ============================================================
 
